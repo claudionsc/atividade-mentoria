e81ef73a3ccf44626d56c3eeeae50b6605f716e3
Claudio Nascimento <claudio.alves20112011@hotmail.com>
2023-11-02 12:03:56 -0300

    feat(src\data\contexts\cartContext\CartContext.tsx - src\data\servicees\AllProducts.tsx): Implementando princípio SOLID(S) - Inversçao de dependência - Iniciando mock de produtos -  @claudionsc

---
+++ src/data/servicees/AllProducts.tsx
@@ -0,0 +1,19 @@
+export type Product = {
+  id: number, 
+  name: string,
+  price:  number,
+  quantity: number
+}
+
+export const allProducts: Array<Product> = [
+    { id: 1, name: "Camiseta", price: 29.99, quantity: 50 },
+    { id: 2, name: "Calça Jeans", price: 59.99, quantity: 30 },
+    { id: 3, name: "Tênis Esportivo", price: 99.99, quantity: 20 },
+    { id: 4, name: "Jaqueta de Couro", price: 149.99, quantity: 10 },
+    { id: 5, name: "Shorts", price: 19.99, quantity: 40 },
+    { id: 6, name: "Sapato Social", price: 79.99, quantity: 25 },
+    { id: 7, name: "Vestido de Festa", price: 129.99, quantity: 15 },
+    { id: 8, name: "Saia", price: 39.99, quantity: 35 },
+    { id: 9, name: "Sapato Casual", price: 69.99, quantity: 30 },
+    { id: 10, name: "Blusa de Frio", price: 49.99, quantity: 45 },
+  ];
