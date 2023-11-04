e8e5e3970e3f9e0bbd9ee12e84adcb1406d9fb57
Claudio Nascimento <claudio.alves20112011@hotmail.com>
2023-11-03 22:14:42 -0300

    test(src\components\products\Products.test.tsx): finalizando teste de integração na página de produtos e no header - @claudionsc

--- src/components/products/Products.test.tsx
+++ src/components/products/Products.test.tsx
@@ -1,17 +1,91 @@
 import "@testing-library/jest-dom";
 import { fireEvent, render, screen } from "@testing-library/react";
-import { describe, expect, test } from "vitest";
+import { describe, expect, test, vi } from "vitest";
 import { Products } from "./Products";
 import { CartContextProvider } from "../../data/contexts/cartContext/CartContext";
+import { Button } from "../button/Button";
+import { Header } from "../header/Header";
 
 describe("Products", () => {
+    
   test("Should render a list of products", () => {
     render(
       <CartContextProvider>
         <Products />
       </CartContextProvider>
-    );
-
-    expect(screen.getAllByTestId("product")).toBeInTheDocument;
+    )
   });
-});
+    
+    expect(screen.queryAllByTestId("product")).toBeInTheDocument;
+
+    test("Should render a button for each list item", () => {
+      render(
+        <CartContextProvider>
+          <Products />
+        </CartContextProvider>
+      );
+      
+      expect(screen.getAllByRole("button")).toBeInTheDocument
+    })
+
+    test("Should able to fire event for each button", () => {
+
+      const handleClick = vi.fn()
+
+      render(<Button onClick={handleClick}>Comprar</Button>)
+
+      fireEvent.click(screen.getByRole("button"))
+      expect(handleClick).toBeCalled()
+    })
+
+    test("Should render a header with text total de produtos", () => {
+      render(
+        <CartContextProvider>
+          <Header />
+        </CartContextProvider>
+      )
+
+        expect(screen.getByText("Total de produtos: 0"))
+
+    })
+
+    test("Should update the product amount when button is clicked ", () => {
+      const handleClick = vi.fn()
+      render(
+        <CartContextProvider>
+          <Header />
+          <Products />
+          <Button onClick={handleClick}>Comprar</Button>
+        </CartContextProvider>
+      )
+
+
+
+      fireEvent.click(screen.getAllByText("Comprar")[0]) 
+      expect(handleClick).toHaveBeenCalledTimes(0)
+
+      expect(screen.getByText("Total de produtos: 1"))
+
+    })
+
+    test("Should update the product amount when button is clicked once again", () => {
+      const handleClick = vi.fn()
+      render(
+        <CartContextProvider>
+          <Header />
+          <Products />
+          <Button onClick={handleClick}>Comprar</Button>
+        </CartContextProvider>
+      )
+
+
+
+      fireEvent.click(screen.getAllByText("Comprar")[0]) 
+      fireEvent.click(screen.getAllByText("Comprar")[1]) 
+      expect(handleClick).toHaveBeenCalledTimes(0)
+      
+      expect(screen.getByText("Total de produtos: 2"))
+
+    })
+    
+  });
